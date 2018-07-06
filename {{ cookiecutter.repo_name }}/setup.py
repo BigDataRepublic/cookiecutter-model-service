3074e5a8c56b76f29195f81a54d0ae61f1c6e4d2
Gerben Oostra <gerben.oostra@bigdatarepublic.nl>
2018-07-06 10:19:39 +0200

    Fix setup.py dependency version requirements

--- {{ cookiecutter.repo_name }}/setup.py
+++ {{ cookiecutter.repo_name }}/setup.py
@@ -16,13 +16,15 @@ setup(
                      "click",
                      "python-dotenv>=0.5.1",
                      "flask==0.12.2",
-                     "python==3",
                      "numpy==1.13.1",
                      "scikit-learn==0.18.1",
                      "dill==0.2.6",
                      "pandas==0.20.3",
                      "scipy==0.19.1",
-                     "git+https://github.com/BigDataRepublic/bdr-engineering-stack.git@develop#subdirectory=data-science-production-container/ds_prod_api",
+                     "ds_prod_api",
+    ],
+    dependency_links=[
+                      "https://github.com/BigDataRepublic/ds-prod-api/tarball/master#egg=ds_prod_api-0.0.1",
     ],
     include_package_data=True,
     package_data={
